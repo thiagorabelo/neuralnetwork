c32bdf4d9ac6333d8a351f42b3f8616b41a097a7
Thiago rabelo <thiagorabelo1@gmail.com>
2019-07-26 12:59:55 -0300

    Reformatando arquivo de exemplo

--- examples/alphabet.py
+++ examples/alphabet.py
@@ -3,11 +3,11 @@ from PIL import Image
 from multilayer_perceptron import MLP, Supervisor, ACTIVATIONS_FUNCTIONS
 
 
-def load_images_data(
-        images_set=('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                    'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                    'y', 'z')):
+def load_images_data():
+    images_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                  'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                  'y', 'z']
     images = [
         (i_name, Image.open(f"./examples/images/alphabet/{i_name}.png"))
         for i_name in images_set
@@ -19,38 +19,10 @@ def load_images_data(
 def convert_data(data):
     chars = [
         list(int(i) for i in s) for s in
-        ('00000',
-         '00001',
-         '00010',
-         '00011',
-         '00100',
-         '00101',
-         '00110',
-         '00111',
-         '01000',
-         '01001',
-         '01010',
-         '01011',
-         '01100',
-         '01101',
-         '01110',
-         '01111',
-         '10000',
-         '10001',
-         '10010',
-         '10011',
-         '10100',
-         '10101',
-         '10110',
-         '10111',
-         '11000',
-         '11001',
-         '11010',
-         '11011',
-         '11100',
-         '11101',
-         '11110',
-         '11111',)
+        ['00000', '00001', '00010', '00011', '00100', '00101', '00110', '00111',
+         '01000', '01001', '01010', '01011', '01100', '01101', '01110', '01111',
+         '10000', '10001', '10010', '10011', '10100', '10101', '10110', '10111',
+         '11000', '11001', '11010', '11011', '11100', '11101', '11110', '11111']
     ]
 
     return [(pixels, answer, name) for (name, pixels), answer in zip(data, chars)]
